f9cdb30d325be0296ac32cfd1c25a496e4c2d7b2
kjxbyz <kjxbyz@163.com>
2023-12-05 22:41:23 +0800

    fix: emoji添加更多字段

--- lib/common/models/emoji_model.g.dart
+++ lib/common/models/emoji_model.g.dart
@@ -10,6 +10,8 @@ _$EmojiModelImpl _$$EmojiModelImplFromJson(Map<String, dynamic> json) =>
     _$EmojiModelImpl(
       id: json['id'] as String?,
       image: json['image'] as String? ?? '',
+      type: json['type'] as String?,
+      size: json['size'] as int?,
       text: json['text'] as String?,
       color: json['color'] as String?,
     );
@@ -18,6 +20,8 @@ Map<String, dynamic> _$$EmojiModelImplToJson(_$EmojiModelImpl instance) =>
     <String, dynamic>{
       'id': instance.id,
       'image': instance.image,
+      'type': instance.type,
+      'size': instance.size,
       'text': instance.text,
       'color': instance.color,
     };
