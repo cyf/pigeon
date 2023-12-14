4ef0c35d4cdb72315d9847d8ca4ab88c4148b005
kjxbyz <kjxbyz888@outlook.com>
2023-12-15 00:54:59 +0800

    fix: remove jpush_flutter

--- plugins/jpush_flutter/ios/Classes/JPushFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface JPushFlutterPlugin : NSObject<FlutterPlugin>
-@end
--- plugins/jpush_flutter/lib/src/jpush_flutter.dart
+++
@@ -1,7 +0,0 @@
-import 'jpush_flutter_platform_interface.dart';
-
-class JPushFlutter {
-  Future<String?> getPlatformVersion() {
-    return JPushFlutterPlatform.instance.getPlatformVersion();
-  }
-}
