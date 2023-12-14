1dbd72e99f9697d3523db91d5077e93cbb1e9ee6
kjxbyz <kjxbyz@163.com>
2023-12-15 00:54:59 +0800

    fix: remove jpush_flutter

--- android/app/src/main/java/com/chenyifaer/homingpigeon/MainApp.java
+++ android/app/src/main/java/com/chenyifaer/homingpigeon/MainApp.java
@@ -1,30 +1,30 @@
 package com.chenyifaer.homingpigeon;
 
 import android.app.Application;
-//import android.os.StrictMode;
+import android.os.StrictMode;
 
-//import com.security.mobile.util.tls12patch.Tls12Patch;
+import com.security.mobile.util.tls12patch.Tls12Patch;
 
 public class MainApp extends Application {
    @Override
    public void onCreate() {
       super.onCreate();
 
-//      Tls12Patch.tls12Patch(getFilesDir());
-//
-//      if (com.hcaptcha.sdk.BuildConfig.DEBUG) {
-//         StrictMode.setThreadPolicy(
-//                 new StrictMode.ThreadPolicy.Builder()
-//                         .detectAll()
-//                         .penaltyLog()
-//                         .build()
-//         );
-//         StrictMode.setVmPolicy(
-//                 new StrictMode.VmPolicy.Builder()
-//                         .detectAll()
-//                         .penaltyLog()
-//                         .build()
-//         );
-//      }
+      Tls12Patch.tls12Patch(getFilesDir());
+
+      if (com.hcaptcha.sdk.BuildConfig.DEBUG) {
+         StrictMode.setThreadPolicy(
+                 new StrictMode.ThreadPolicy.Builder()
+                         .detectAll()
+                         .penaltyLog()
+                         .build()
+         );
+         StrictMode.setVmPolicy(
+                 new StrictMode.VmPolicy.Builder()
+                         .detectAll()
+                         .penaltyLog()
+                         .build()
+         );
+      }
    }
 }
--- plugins/jpush_flutter/lib/jpush_flutter.dart
+++
@@ -1,1 +0,0 @@
-export 'src/jpush_flutter.dart';
