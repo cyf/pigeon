ca58b308a9b6d13f1e13dd7fccb60202eb64ae8c
kjxbyz <kjxbyz@163.com>
2023-12-24 19:07:57 +0800

    feat: add socket support

--- lib/main_internal.dart
+++ lib/main_internal.dart
@@ -8,13 +8,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:homing_pigeon/app/bloc_observer.dart';
 import 'package:homing_pigeon/app/config.dart';
 import 'package:homing_pigeon/common/constants/constants.dart';
+import 'package:homing_pigeon/common/logger/logger.dart';
 import 'package:homing_pigeon/common/utils/sp_util.dart';
 import 'package:homing_pigeon/main_common.dart';
 import 'package:homing_pigeon/modules/app/app.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:socket_io_client/socket_io_client.dart' as socket_io;
 import 'package:timezone/data/latest_10y.dart' as tz;
 
 Future<void> main() async {
@@ -26,13 +29,40 @@ Future<void> main() async {
 
   if (Constants.sentryEnabled && kReleaseMode) {
     await SentryFlutter.init(
-          (options) {
+      (options) {
         options
           ..dsn = Constants.sentryDsn
           ..tracesSampleRate = 1.0;
       },
     );
   }
+
+  final socket = socket_io.io(
+    '${Constants.apiPrefix}/ws',
+    socket_io.OptionBuilder()
+        .setTransports(['websocket']).setExtraHeaders({'foo': 'bar'}).build(),
+  );
+
+  socket
+    ..onConnecting(printDebugLog)
+    ..onConnect((_) {
+      printDebugLog('connect');
+      Fluttertoast.showToast(msg: 'Connected', gravity: ToastGravity.BOTTOM);
+      socket.emit('msg', 'test');
+    })
+    ..onConnectError(printErrorLog)
+    ..onConnectTimeout(printErrorLog)
+    ..on('event', printDebugLog)
+    ..onReconnectAttempt(printDebugLog)
+    ..onReconnecting(printDebugLog)
+    ..onReconnect((_) {
+      printDebugLog('connect');
+      Fluttertoast.showToast(msg: 'Connected', gravity: ToastGravity.BOTTOM);
+    })
+    ..onReconnectError(printErrorLog)
+    ..onReconnectFailed(printErrorLog)
+    ..onDisconnect((_) => printDebugLog('disconnect'))
+    ..onError(printErrorLog);
 
   if (Constants.ossEnabled) {
     OSSClient.init(
