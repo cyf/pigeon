8e1d313e7254d21665a20c5a3cdc48608f592588
kjxbyz <kjxbyz888@outlook.com>
2024-02-24 15:21:25 +0800

    fix: update constants

--- node/docs/constants/index.ts
+++ node/docs/constants/index.ts
@@ -4,7 +4,7 @@ export const cacheLngKey: string = "__homing_pigeon_lng__";
 export const cacheTokenKey: string = "__homing_pigeon_token__";
 export const cacheThemeKey: string = "__homing_pigeon_theme__";
 export const host =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? "https://www.chenyifaer.com"
     : "http://localhost:3000";
 export const basePath =
