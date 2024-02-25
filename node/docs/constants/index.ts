7b618c478375046d5b7a14339f9c256d1ab6e9cc
kjxbyz <kjxbyz@163.com>
2024-02-25 20:25:21 +0800

    fix: update sitemap.ts

--- node/docs/constants/index.ts
+++ node/docs/constants/index.ts
@@ -3,14 +3,15 @@ import { SystemOS } from "@/types/common";
 export const cacheLngKey: string = "__homing_pigeon_lng__";
 export const cacheTokenKey: string = "__homing_pigeon_token__";
 export const cacheThemeKey: string = "__homing_pigeon_theme__";
+export const basePath =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/homing-pigeon" : "";
 export const host =
   process.env.NODE_ENV === "production"
     ? "https://www.chenyifaer.com"
     : "http://localhost:3000";
-export const basePath =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/homing-pigeon" : "";
+export const domain = `${host}${basePath}`;
 export const platforms: Record<SystemOS, string[]> = {
   ios: [".ipa"],
   android: [".apk", ".aab"],
 };
-export const pageSize: number = 15;
+export const pageSize: number = 10;
