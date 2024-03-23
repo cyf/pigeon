5c3ce0d80a47b151a52c16b06ac43f6291386d1a
kjxbyz <kjxbyz@163.com>
2024-03-23 20:06:37 +0800

    refactor: update ui components

--- node/docs/components/ui/input.tsx
+++ node/docs/components/ui/input.tsx
@@ -8,41 +8,40 @@ import { cn } from "@/lib/utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    const [showPassword, setShowPassword] = React.useState(false);
+const Input = React.forwardRef<
+  HTMLInputElement,
+  InputProps & { wrapperClassName?: string }
+>(({ wrapperClassName, className, type, ...props }, ref) => {
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
-    return (
-      <div className="relative">
-        <input
-          type={
-            type === "password" ? (showPassword ? "text" : "password") : type
-          }
-          className={cn(
-            "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-        {type === "password" && (
-          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
-            {showPassword ? (
-              <EyeIcon className="h-4 w-4" onClick={togglePasswordVisibility} />
-            ) : (
-              <EyeOffIcon
-                className="h-4 w-4"
-                onClick={togglePasswordVisibility}
-              />
-            )}
-          </div>
+  return (
+    <div className={`relative ${wrapperClassName || ""}`}>
+      <input
+        type={type === "password" ? (showPassword ? "text" : "password") : type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
         )}
-      </div>
-    );
-  },
-);
+        ref={ref}
+        {...props}
+      />
+      {type === "password" && (
+        <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
+          {showPassword ? (
+            <EyeIcon className="h-4 w-4" onClick={togglePasswordVisibility} />
+          ) : (
+            <EyeOffIcon
+              className="h-4 w-4"
+              onClick={togglePasswordVisibility}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+});
 Input.displayName = "Input";
 
 const FileInput = React.forwardRef<
@@ -54,7 +53,7 @@ const FileInput = React.forwardRef<
       {children}
       <input
         className={cn(
-          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
           "hidden",
         )}
