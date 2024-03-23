10e5de838cc295c7930d952275fb1b661fbd5761
kjxbyz <kjxbyz@163.com>
2024-03-23 20:34:42 +0800

    fix: update i18n

--- node/docs/components/home/github-release.tsx
+++ node/docs/components/home/github-release.tsx
@@ -132,7 +132,7 @@ export default function GithubRelease({ lng }: LngProps) {
             href={`/${lng}/releases`}
             className="ml-2 text-sm text-gray-500 hover:underline dark:text-gray-400"
           >
-            More releases
+            {t("more-versions")}
           </Link>
         </Balancer>
       </p>
