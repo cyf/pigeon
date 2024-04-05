9e63b4ab24fbce48f7d660332ab3c575aefa416d
kjxbyz <kjxbyz@163.com>
2024-04-05 20:42:00 +0800

    fix: update font size

--- node/docs/app/[lng]/[type]/[slug]/page.tsx
+++ node/docs/app/[lng]/[type]/[slug]/page.tsx
@@ -24,7 +24,9 @@ export default function Legal({
           {/* Article header */}
           <header className="mx-auto mb-20 max-w-3xl">
             {/* Title */}
-            <h1 className="h1 mb-4 text-center">{post.title}</h1>
+            <h1 className="h1 mb-4 text-center text-5xl font-bold">
+              {post.title}
+            </h1>
           </header>
 
           {/* Article content */}
