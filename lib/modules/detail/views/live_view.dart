b9c72bbf1a49df892c4655da23af5ea61444713a
kjxbyz <kjxbyz888@outlook.com>
2024-08-16 23:54:10 +0800

    Update LiveView

--- lib/modules/detail/views/live_view.dart
+++ lib/modules/detail/views/live_view.dart
@@ -1,23 +1,19 @@
 // Flutter imports:
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:gap/gap.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 // Project imports:
 import 'package:pigeon/app/navigator.dart';
 import 'package:pigeon/common/enums/enums.dart';
 import 'package:pigeon/common/extensions/single.dart';
 import 'package:pigeon/common/http/utils/handle_errors.dart';
-import 'package:pigeon/common/utils/string_util.dart';
 import 'package:pigeon/common/widgets/widgets.dart';
 import 'package:pigeon/i18n/i18n.dart';
-import 'package:pigeon/theme/colors.dart';
 
 class LiveView extends StatefulWidget {
   const LiveView({super.key});
@@ -31,13 +27,6 @@ class _LiveViewState extends State<LiveView> {
     controlFinishRefresh: true,
     controlFinishLoad: true,
   );
-  late YoutubePlayerController _youtubePlayerController;
-
-  PlayerState? _playerState;
-  YoutubeMetaData? _videoMetaData;
-  double _volume = 100;
-  bool _muted = false;
-  bool _isPlayerReady = false;
 
   int currentPage = 1;
   int totalResults = 0;
@@ -45,27 +34,12 @@ class _LiveViewState extends State<LiveView> {
   @override
   void initState() {
     super.initState();
-    _youtubePlayerController = YoutubePlayerController(
-      initialVideoId: 'iLnmTe5Q2Qw',
-      flags: const YoutubePlayerFlags(
-        autoPlay: false,
-        mute: true,
-      ),
-    )..addListener(listener);
-
     // _load();
-  }
-
-  @override
-  void deactivate() {
-    _youtubePlayerController.pause();
-    super.deactivate();
   }
 
   @override
   void dispose() {
     _easyRefreshController.dispose();
-    _youtubePlayerController.dispose();
     super.dispose();
   }
 
@@ -74,79 +48,39 @@ class _LiveViewState extends State<LiveView> {
     final t = Translations.of(context);
     final bottom = MediaQuery.paddingOf(context).bottom;
     final isDark = Theme.of(context).brightness == Brightness.dark;
-    return YoutubePlayerBuilder(
-      onEnterFullScreen: () {
-        SystemChrome.setPreferredOrientations([
-          DeviceOrientation.landscapeRight,
-        ]);
-      },
-      onExitFullScreen: () {
-        SystemChrome.setPreferredOrientations([
-          DeviceOrientation.portraitUp,
-        ]);
-      },
-      player: YoutubePlayer(
-        controller: _youtubePlayerController,
-        showVideoProgressIndicator: true,
-        progressIndicatorColor: primaryColor,
-        progressColors: const ProgressBarColors(
-          playedColor: primaryColor,
-          handleColor: primaryHoverColor,
-        ),
-        onReady: () {
-          setState(() => _isPlayerReady = true);
-        },
+    return Scaffold(
+      appBar: HpAppBar(
+        isDark: isDark,
+        titleName: t.pages.live.title,
       ),
-      builder: (context, player) {
-        return Scaffold(
-          appBar: HpAppBar(
-            isDark: isDark,
-            titleName: StringUtil.getValue(
-              _videoMetaData?.title,
-              defaultVal: t.pages.live.title,
-            ),
-          ),
-          body: EasyRefresh(
-            controller: _easyRefreshController,
-            header: const ClassicHeader(),
-            // onRefresh: () => _load(operation: Operation.refresh),
-            // onLoad: () => _load(operation: Operation.load),
-            child: ListView(
-              children: [
-                // some widgets
-                player,
-                //some other widgets
-                if (kDebugMode) ...[
-                  const Gap(10),
-                  ElevatedButton(
-                    onPressed: _load,
-                    child: Text(t.buttons.login),
-                  ),
-                ],
-              ],
-            ),
-          ).nestedPadding(
-            padding: EdgeInsets.only(
-              top: 10,
-              left: 10,
-              right: 10,
-              bottom: bottom,
-            ),
-          ),
-        );
-      },
+      body: EasyRefresh(
+        controller: _easyRefreshController,
+        header: const ClassicHeader(),
+        // onRefresh: () => _load(operation: Operation.refresh),
+        // onLoad: () => _load(operation: Operation.load),
+        child: ListView(
+          children: [
+            // some widgets
+            const Text('Live'),
+            //some other widgets
+            if (kDebugMode) ...[
+              const Gap(10),
+              ElevatedButton(
+                onPressed: _load,
+                child: Text(t.buttons.login),
+              ),
+            ],
+          ],
+        ),
+      ).nestedPadding(
+        padding: EdgeInsets.only(
+          top: 10,
+          left: 10,
+          right: 10,
+          bottom: 10 + bottom,
+        ),
+      ),
     );
-  }
-
-  void listener() {
-    if (_isPlayerReady &&
-        mounted &&
-        !_youtubePlayerController.value.isFullScreen) {
-      setState(() {
-        _playerState = _youtubePlayerController.value.playerState;
-        _videoMetaData = _youtubePlayerController.metadata;
-      });
-    }
   }
 
   Future<void> _load({
